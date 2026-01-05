c1c53b51d80416ce42812b408570e8722864c1c1
elli-ma <elli9225@gmail.com>
2026-01-05 19:22:22 +0300

    add robot.txt and sitemap.ts

---
+++ src/app/sitemap.ts
@@ -0,0 +1,21 @@
+import { recipes } from "@/lib/recipes";
+
+export const dynamic = "force-static"; 
+
+export default async function sitemap() {
+  const baseUrl = "https://family-cook.ru"; // замени на свой домен
+
+  // общие страницы
+  const staticPages = ["", "about", "contacts"].map((page) => ({
+    url: `${baseUrl}/${page}/`,
+    lastModified: new Date().toISOString(),
+  }));
+
+  // страницы рецептов
+  const recipePages = recipes.map((recipe) => ({
+    url: `${baseUrl}/recipes/${recipe.slug}/`,
+    lastModified: new Date().toISOString(),
+  }));
+
+  return [...staticPages, ...recipePages];
+}
